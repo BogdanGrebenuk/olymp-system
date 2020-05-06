4c6e356da1994d303e870e21a2086890f43bc940
BogdanGrebenuk <bogdan020699@gmail.com>
2020-05-06 21:13:48 +0300

    add user registration, authentication and authorization

--- app/main.py
+++ app/main.py
@@ -6,14 +6,22 @@ from aiohttp import web
 from db.utils import init_pg, close_pg
 from commandbus import Bus
 from common import PUBLIC_DIR
-from middlewares import error_middleware
+from middlewares import (
+    error_handler,
+    user_injector
+)
 from routes import setup_routes
 from settings import config
 from utils.executor import init_pools, close_pools
 
 
 if __name__ == '__main__':
-    app = web.Application(middlewares=[error_middleware])
+    app = web.Application(
+        middlewares=[
+            error_handler,
+            user_injector
+        ]
+    )
 
     setup_routes(app)
     app.add_routes([web.static('/public', PUBLIC_DIR)])
