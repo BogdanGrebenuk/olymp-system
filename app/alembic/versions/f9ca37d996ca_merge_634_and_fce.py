c38caaf1fe86344ceb9e9795c57534e988241158
BogdanGrebenuk <bogdan020699@gmail.com>
2020-05-09 01:53:17 +0300

    merge alembic heads

---
+++ app/alembic/versions/f9ca37d996ca_merge_634_and_fce.py
@@ -0,0 +1,24 @@
+"""merge 634 and fce
+
+Revision ID: f9ca37d996ca
+Revises: 634791316640, fcee19b76917
+Create Date: 2020-05-09 01:47:19.118784
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f9ca37d996ca'
+down_revision = ('634791316640', 'fcee19b76917')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
